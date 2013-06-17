a2e4c204a58342447188e6dc8afa38c042a77db7
Nick Reid <nickreid@nickreid.com>
2013-06-17 10:32:14 -0700

    added super basic list view

--- directory/members/views.py
+++ directory/members/views.py
@@ -1,1 +1,17 @@
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseBadRequest, Http404, HttpResponseRedirect
+from django.template import RequestContext
+from django.core.urlresolvers import reverse
+
+from django.views.decorators.csrf import csrf_protect
+
+from members.models import Member
+
+from django.contrib import messages
+
+def list(request):
+	members = Member.objects.all()
+	#randomize
+	return render(request, 'members/list.html',{
+			'members': members,
+		})
