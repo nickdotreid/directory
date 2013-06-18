b6c1883d2b314d6f1afd102031fd7502dfd6662c
Nick Reid <nickreid@nickreid.com>
2013-06-17 18:25:48 -0700

    added basic form

--- directory/members/urls.py
+++ directory/members/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('members.views',
+	url(r'^create','edit'),
+	url(r'^(?P<key>\w+)/edit','edit'),
 	url(r'^(?P<key>\w+)','detail'),
 	url(r'^','list'),
 )
--- directory/members/views.py
+++ directory/members/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, get_object_or_404
+from annoying.functions import get_object_or_None
 from django.http import HttpResponse, HttpResponseBadRequest, Http404, HttpResponseRedirect
 from django.template import RequestContext
 from django.core.urlresolvers import reverse
 
 from django.views.decorators.csrf import csrf_protect
+
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 
 from members.models import Member
 
@@ -18,6 +22,34 @@ def detail(request, key, others=4):
 		'member':member,
 		'members':members,
 	})
+
+def edit(request, key=None):
+	member = get_object_or_None(Member, key=key)
+	MemberForm = make_edit_form()
+	form = None
+	if member:
+		form = MemberForm(instance=member)
+	else:
+		form = MemberForm()
+	return render(request, 'members/form.html', {
+		'member': member,
+		'form':form
+		})
+
+from django.forms import ModelForm
+def make_edit_form():
+	class MemberForm(ModelForm):
+		def __init__(self, *args, **kwargs):
+			super(ModelForm, self).__init__(*args, **kwargs)
+			self.helper = FormHelper()
+
+			self.helper.add_input(Submit('submit', 'Save'))
+		class Meta:
+			model = Member
+			fields = ['name', 'email', 'title', 'website']
+	return MemberForm
+
+
 
 def list(request):
 	members = Member.objects.all()
