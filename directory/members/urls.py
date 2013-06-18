02c8a8a8ce12f4b8680e360eb9b84fe45e72d741
Nick Reid <nickreid@nickreid.com>
2013-06-17 18:45:46 -0700

    removed /edit form edit url

--- directory/members/urls.py
+++ directory/members/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('members.views',
 	url(r'^create','edit'),
-	url(r'^(?P<key>\w+)/edit','edit'),
+	url(r'^(?P<key>\w+)','edit'),
 	url(r'^','list'),
 )
