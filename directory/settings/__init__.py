42758791cece6a8bf80b85615f888b73d568ffb3
Nick Reid <nickreid@nickreid.com>
2013-06-19 11:36:12 -0700

    added gravator

--- directory/settings/__init__.py
+++ directory/settings/__init__.py
@@ -124,6 +124,7 @@ INSTALLED_APPS = (
     'south',
     'annoying',
     'crispy_forms',
+    'gravatar',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
